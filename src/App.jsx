f8de8b8860cf2a07c3646a4fcba143212ec53e45
abdurrashidkhan <abdurrashidkhan.personal@gmail.com>
2025-07-30 12:28:19 +0600

    config redux, tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+
+  return (
+    <div className="bg-amber-300">
+      <h1 className='text-5xl'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
